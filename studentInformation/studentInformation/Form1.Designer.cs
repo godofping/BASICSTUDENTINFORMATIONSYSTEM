53c99352015c182af5be6e1031ea7c2f1ab4ca78
rexy <rexmicrosoft@yahoo.com.ph>
2019-07-20 01:06:30 +0800

    practice

--- studentInformation/studentInformation/Form1.Designer.cs
+++ studentInformation/studentInformation/Form1.Designer.cs
@@ -28,92 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.b_submit = new System.Windows.Forms.Button();
             this.l_firstName = new System.Windows.Forms.Label();
             this.tb_firstName = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb_lastName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.cb_section = new System.Windows.Forms.ComboBox();
+            this.dg_students = new System.Windows.Forms.DataGridView();
+            this.b_clear = new System.Windows.Forms.Button();
+            this.b_update = new System.Windows.Forms.Button();
+            this.b_delete = new System.Windows.Forms.Button();
+            this.b_add = new System.Windows.Forms.Button();
+            this.b_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dg_students)).BeginInit();
             this.SuspendLayout();
-            // 
-            // b_submit
-            // 
-            this.b_submit.Location = new System.Drawing.Point(98, 173);
-            this.b_submit.Name = "b_submit";
-            this.b_submit.Size = new System.Drawing.Size(75, 23);
-            this.b_submit.TabIndex = 0;
-            this.b_submit.Text = "Submit";
-            this.b_submit.UseVisualStyleBackColor = true;
-            this.b_submit.Click += new System.EventHandler(this.B_submit_Click);
             // 
             // l_firstName
             // 
             this.l_firstName.AutoSize = true;
-            this.l_firstName.Location = new System.Drawing.Point(12, 39);
+            this.l_firstName.Location = new System.Drawing.Point(9, 73);
+            this.l_firstName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.l_firstName.Name = "l_firstName";
-            this.l_firstName.Size = new System.Drawing.Size(80, 17);
+            this.l_firstName.Size = new System.Drawing.Size(60, 13);
             this.l_firstName.TabIndex = 1;
             this.l_firstName.Text = "First Name:";
             // 
             // tb_firstName
             // 
-            this.tb_firstName.Location = new System.Drawing.Point(98, 36);
+            this.tb_firstName.Location = new System.Drawing.Point(74, 70);
+            this.tb_firstName.Margin = new System.Windows.Forms.Padding(2);
             this.tb_firstName.Name = "tb_firstName";
-            this.tb_firstName.Size = new System.Drawing.Size(288, 22);
+            this.tb_firstName.Size = new System.Drawing.Size(217, 20);
             this.tb_firstName.TabIndex = 2;
             // 
-            // textBox1
+            // tb_lastName
             // 
-            this.textBox1.Location = new System.Drawing.Point(98, 72);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(288, 22);
-            this.textBox1.TabIndex = 4;
+            this.tb_lastName.Location = new System.Drawing.Point(74, 99);
+            this.tb_lastName.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_lastName.Name = "tb_lastName";
+            this.tb_lastName.Size = new System.Drawing.Size(217, 20);
+            this.tb_lastName.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Location = new System.Drawing.Point(11, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "First Name:";
+            this.label1.Text = "Last Name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 119);
+            this.label2.Location = new System.Drawing.Point(11, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "First Name:";
+            this.label2.Text = "Section:";
+            // 
+            // cb_section
+            // 
+            this.cb_section.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_section.FormattingEnabled = true;
+            this.cb_section.Location = new System.Drawing.Point(74, 130);
+            this.cb_section.Name = "cb_section";
+            this.cb_section.Size = new System.Drawing.Size(217, 21);
+            this.cb_section.TabIndex = 6;
+            // 
+            // dg_students
+            // 
+            this.dg_students.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg_students.Location = new System.Drawing.Point(339, 32);
+            this.dg_students.Name = "dg_students";
+            this.dg_students.Size = new System.Drawing.Size(378, 239);
+            this.dg_students.TabIndex = 7;
+            // 
+            // b_clear
+            // 
+            this.b_clear.Location = new System.Drawing.Point(235, 169);
+            this.b_clear.Margin = new System.Windows.Forms.Padding(2);
+            this.b_clear.Name = "b_clear";
+            this.b_clear.Size = new System.Drawing.Size(56, 19);
+            this.b_clear.TabIndex = 8;
+            this.b_clear.Text = "Clear";
+            this.b_clear.UseVisualStyleBackColor = true;
+            this.b_clear.Click += new System.EventHandler(this.B_clear_Click);
+            // 
+            // b_update
+            // 
+            this.b_update.Location = new System.Drawing.Point(156, 32);
+            this.b_update.Margin = new System.Windows.Forms.Padding(2);
+            this.b_update.Name = "b_update";
+            this.b_update.Size = new System.Drawing.Size(56, 19);
+            this.b_update.TabIndex = 9;
+            this.b_update.Text = "Update";
+            this.b_update.UseVisualStyleBackColor = true;
+            // 
+            // b_delete
+            // 
+            this.b_delete.Location = new System.Drawing.Point(235, 32);
+            this.b_delete.Margin = new System.Windows.Forms.Padding(2);
+            this.b_delete.Name = "b_delete";
+            this.b_delete.Size = new System.Drawing.Size(56, 19);
+            this.b_delete.TabIndex = 10;
+            this.b_delete.Text = "Delete";
+            this.b_delete.UseVisualStyleBackColor = true;
+            // 
+            // b_add
+            // 
+            this.b_add.Location = new System.Drawing.Point(74, 32);
+            this.b_add.Margin = new System.Windows.Forms.Padding(2);
+            this.b_add.Name = "b_add";
+            this.b_add.Size = new System.Drawing.Size(56, 19);
+            this.b_add.TabIndex = 11;
+            this.b_add.Text = "Add";
+            this.b_add.UseVisualStyleBackColor = true;
+            this.b_add.Click += new System.EventHandler(this.b_add_Click);
+            // 
+            // b_save
+            // 
+            this.b_save.Location = new System.Drawing.Point(74, 169);
+            this.b_save.Margin = new System.Windows.Forms.Padding(2);
+            this.b_save.Name = "b_save";
+            this.b_save.Size = new System.Drawing.Size(56, 19);
+            this.b_save.TabIndex = 12;
+            this.b_save.Text = "Save";
+            this.b_save.UseVisualStyleBackColor = true;
+            this.b_save.Click += new System.EventHandler(this.B_save_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(729, 295);
+            this.Controls.Add(this.b_save);
+            this.Controls.Add(this.b_add);
+            this.Controls.Add(this.b_delete);
+            this.Controls.Add(this.b_update);
+            this.Controls.Add(this.b_clear);
+            this.Controls.Add(this.dg_students);
+            this.Controls.Add(this.cb_section);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_lastName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tb_firstName);
             this.Controls.Add(this.l_firstName);
-            this.Controls.Add(this.b_submit);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BASIC STUDENT INFORMATION SYSTEM";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg_students)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button b_submit;
         private System.Windows.Forms.Label l_firstName;
         private System.Windows.Forms.TextBox tb_firstName;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tb_lastName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cb_section;
+        private System.Windows.Forms.DataGridView dg_students;
+        private System.Windows.Forms.Button b_clear;
+        private System.Windows.Forms.Button b_update;
+        private System.Windows.Forms.Button b_delete;
+        private System.Windows.Forms.Button b_add;
+        private System.Windows.Forms.Button b_save;
     }
 }
 
--- studentInformation/studentInformation/Form1.cs
+++ studentInformation/studentInformation/Form1.cs
@@ -8,14 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
+
 namespace studentInformation
 
 {
     public partial class Form1 : Form
     {
 
-        string firstname = "";
-
+        string connectionString = @"Server=localhost;Database=student_information_db;Uid=root;Pwd=;";
 
         public Form1()
         {
@@ -24,14 +25,59 @@ namespace studentInformation
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+            clearFields();
+            loadSections();
         }
 
-        private void B_submit_Click(object sender, EventArgs e)
+
+        private void B_clear_Click(object sender, EventArgs e)
         {
-            firstname = tb_firstName.Text;
-            MessageBox.Show(firstname);
-           
+            clearFields();
+        }
+
+
+        private void clearFields()
+        {
+            tb_firstName.Clear();
+            tb_lastName.Clear();
+            cb_section.ResetText();
+
+        }
+
+
+        private void loadSections()
+        {
+            using (MySqlConnection c = new MySqlConnection(connectionString))
+            {
+                c.Open();
+
+                var sql = "SELECT * from section_table";
+                using (MySqlCommand cmd = new MySqlCommand(sql, c))
+                {
+                    var dt = new DataTable();
+                    dt.Load(cmd.ExecuteReader());
+
+                    cb_section.ValueMember = "sectionId";
+                    cb_section.DisplayMember = "section";
+                    cb_section.DataSource = dt;
+                }
+            }
+        }
+
+        private void B_save_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("studentAddorEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                //mySqlCmd.Parameters.AddWithValue("");
+            }
+        }
+
+        private void b_add_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cb_section.SelectedValue.ToString());
         }
     }
 }
